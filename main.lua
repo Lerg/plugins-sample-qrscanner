b5d5411fcce20ccb2f013e5dee06c0721e129993
Lerg <lvs.tourist@gmail.com>
2016-03-21 07:11:10 +0500

    Added options.symbols to specify which code types to search.

--- main.lua
+++ main.lua
@@ -8,13 +8,24 @@ qrscanner.show(listner, [options])
 
 Opens the scanner view.
 
+options.symbols - array of strings, a list of all types of visual codes to search. Default is 'qr'.
 options.strings - key-value table of string-string values for customization/localization.
 
 Example:
 
 qrscanner.show(listener, {strings = {title = 'My Custom Title'}})
 
-All default values:
+Symbols possible values:
+Android:
+'code39', 'code93', 'code128', 'codabar', 'databar', 'databar_exp', 'ean8', 'ean13',
+'i25', 'isbn10', 'isbn13', 'partial', 'pdf417', 'qr', 'upca', 'upce'
+iOS:
+'aztec', 'code39', 'code39mod43', 'code93', 'code128', 'datamatrix', 'ean8', 'ean13',
+'interleaved2of5', 'itf14', 'pdf417', 'qr', 'upce'
+
+'interleaved2of5', 'itf14' and 'datamatrix' require iOS 8.0+.
+
+All default values for strings:
 
 options.strings = {
     title = 'QR Code Scanner'
@@ -29,19 +40,38 @@ On Android only title and no_camera_err are used.
 
 ]]
 
-local rect = display.newRect(display.contentCenterX, display.contentCenterY, 100, 100)
+local rect = display.newRect(display.contentCenterX, display.contentCenterY, 200, 200)
 rect:setFillColor(0.75)
 
 local function listener(message)
     -- message variable contains the value of a scanned QR Code or a barcode.
+    print('Scanned message:', message)
     native.showAlert('QR Code Scanner', message, {'OK'})
 end
 
 local scanButton = widget.newButton {
-    x = rect.x, y = rect.y,
-    width = 75, height = 50,
-    label = 'Scan',
+    x = rect.x, y = rect.y - 40,
+    width = 150, height = 50,
+    label = 'Scan QR',
     onRelease = function()
-        print('Showing QR')
+        print('Showing scanner')
         qrscanner.show(listener)
+    end}
+
+local scanButton = widget.newButton {
+    x = rect.x, y = rect.y + 40,
+    width = 150, height = 50,
+    label = 'Scan Bar',
+    onRelease = function()
+        print('Showing scanner')
+        qrscanner.show(listener, {
+            symbols = {
+                'aztec', 'code39', 'code39mod43', 'code93', 'code128', 'codabar',
+                'databar', 'databar_exp', 'datamatrix', 'ean8', 'ean13', 'interleaved2of5',
+                'itf14', 'i25', 'isbn10', 'isbn13', 'partial', 'pdf417', 'upca', 'upce'
+            },
+            strings = {
+                title = 'Barcode Scanner'
+            }    
+        })
     end}
